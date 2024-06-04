501249612be1d0d251c2aa381efc7883b5d1dab5
josugoar <josugoar@gmail.com>
2024-06-04 21:52:02 +0200

    Refactor predict method in MonoGpTest class

--- monogp/monogp_test.py
+++ monogp/monogp_test.py
@@ -18,41 +18,41 @@ class MonoGpTest(Base3DDetector):
         raise NotImplementedError
 
     def predict(self, batch_inputs, batch_data_samples):
-        results = []
-        for batch_data_sample in batch_data_samples:
-            metainfo = batch_data_sample.metainfo
-            box_type_3d = metainfo['box_type_3d']
-            cam2img = metainfo['cam2img']
-            plane = metainfo['plane']
+        result_list_3d = []
 
-            eval_ann_info = batch_data_sample.eval_ann_info
-            bboxes_3d = eval_ann_info['gt_bboxes_3d']
+        for batch_data_sample in batch_data_samples:
+            bboxes_3d = batch_data_sample.eval_ann_info['gt_bboxes_3d']
             labels_3d = bboxes_3d.tensor.new_tensor(
-                eval_ann_info['gt_labels_3d'], dtype=torch.long)
-            scores_3d = torch.ones_like(labels_3d)
+                batch_data_sample.eval_ann_info['gt_labels_3d'],
+                dtype=torch.long)
+            scores_3d = bboxes_3d.tensor.new_ones(labels_3d.shape)
+            cam2img = batch_data_sample.metainfo['cam2img']
+            plane = batch_data_sample.metainfo['plane']
+            box_type_3d = batch_data_sample.metainfo['box_type_3d']
+
             if self.origin[1] == 0.5:
                 centers_2d = points_cam2img(bboxes_3d.gravity_center, cam2img)
             elif self.origin[1] == 1.0:
                 centers_2d = points_cam2img(bboxes_3d.bottom_center, cam2img)
             else:
                 raise ValueError(f'Unsupported origin {self.origin}')
-
             shift_height = plane[3] - bboxes_3d.bottom_height
-            height = bboxes_3d.height
-            centers_3d = points_img2plane(centers_2d, cam2img, plane,
-                                          shift_height, height, self.origin)
-            bboxes_3d.tensor[:, :3] = centers_3d
+            bboxes_3d.tensor[:, :3] = points_img2plane(centers_2d, cam2img,
+                                                       plane, shift_height,
+                                                       bboxes_3d.height,
+                                                       self.origin)
 
             result = InstanceData()
             result.bboxes_3d = box_type_3d(bboxes_3d.tensor,
                                            origin=self.origin)
             result.labels_3d = labels_3d
             result.scores_3d = scores_3d
-            results.append(result)
+            result_list_3d.append(result)
 
-        batch_data_samples = self.add_pred_to_datasample(
-            batch_data_samples, results)
-        return batch_data_samples
+        predictions = self.add_pred_to_datasample(batch_data_samples,
+                                                  result_list_3d)
+
+        return predictions
 
     def _forward(self, batch_inputs, batch_data_samples=None):
         raise NotImplementedError
