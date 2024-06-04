2711fb1804bc3b43b2e9402937d7aedab384cd3a
josugoar <josugoar@gmail.com>
2024-06-04 21:51:54 +0200

    Refactor points_img2plane function to remove unused code and update DEPTH value

--- monogp/utils.py
+++ monogp/utils.py
@@ -7,14 +7,11 @@ from torch import Tensor
 from mmdet3d.structures import points_img2cam
 from mmdet3d.utils import array_converter
 
+DEPTH = 10
 
-@array_converter(apply_to=('centers_2d', 'cam2img', 'plane'))
+
+@array_converter(apply_to=('centers_2d', 'cam2img'))
 def points_img2plane(centers_2d, cam2img, plane, shift_height, height, origin):
-    # Arbitrary depth value representing the scale to a 3D plane in the camera
-    # coordinate system. Careful with its magnitude, as a low value may cause
-    # numerical instability
-    DEPTH = 100
-
     shift_height += height * (1.0 - origin[1])
 
     num_points = centers_2d.shape[0]
@@ -23,5 +20,4 @@ def points_img2plane(centers_2d, cam2img, plane, shift_height, height, origin):
     points3D = points_img2cam(points, cam2img)
     scale = torch.unsqueeze((plane[3] - shift_height) / points3D[:, 1], 1)
     centers_3d = scale * points3D
-
     return centers_3d
