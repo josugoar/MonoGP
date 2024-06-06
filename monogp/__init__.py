b1d0e8a0f6b9fe6d67bd898abb01136a8a382614
josugoar <josugoar@gmail.com>
2024-06-06 04:59:51 +0200

    Add new classes and functions for 3D object detection

--- monogp/__init__.py
+++ monogp/__init__.py
@@ -1,15 +1,15 @@
+from .ground_plane_kitti_metric import GroundPlaneKittiMetric
 from .monogp_fcos3d_bbox_coder import MonoGpFCOS3DBBoxCoder
 from .monogp_fcos3d_mono3d_head import MonoGpFCOS3DMono3DHead
 from .monogp_smoke_bbox_coder import MonoGpSMOKECoder
 from .monogp_smoke_mono3d_head import MonoGpSMOKEMono3DHead
 from .monogp_test import MonoGpTest
 from .transforms_3d import (BBoxes3DToBBoxes, BottomCenterToCenters2DWithDepth,
-                            ObjectPlaneAlignment)
-from .kitti_metric import MonoGpKittiMetric
+                            ObjectPlaneAlignment, ObjectShiftHeight)
 
 __all__ = [
-    'MonoGpFCOS3DBBoxCoder', 'MonoGpFCOS3DMono3DHead', 'MonoGpSMOKECoder',
-    'MonoGpSMOKEMono3DHead', 'MonoGpTest', 'BBoxes3DToBBoxes',
-    'BottomCenterToCenters2DWithDepth', 'ObjectPlaneAlignment',
-    'MonoGpKittiMetric'
+    'GroundPlaneKittiMetric', 'MonoGpFCOS3DBBoxCoder',
+    'MonoGpFCOS3DMono3DHead', 'MonoGpSMOKECoder', 'MonoGpSMOKEMono3DHead',
+    'MonoGpTest', 'BBoxes3DToBBoxes', 'BottomCenterToCenters2DWithDepth',
+    'ObjectPlaneAlignment', 'ObjectShiftHeight'
 ]
--- monogp/transforms_3d.py
+++ monogp/transforms_3d.py
@@ -1,3 +1,4 @@
+import torch
 from mmcv.transforms import BaseTransform
 
 from mmdet3d.registry import TRANSFORMS
@@ -23,11 +24,11 @@ class BBoxes3DToBBoxes(BaseTransform):
 class BottomCenterToCenters2DWithDepth(BaseTransform):
 
     def transform(self, input_dict: dict) -> dict:
-        gt_bboxes_3d = input_dict['gt_bboxes_3d']
+        bboxes_3d = input_dict['gt_bboxes_3d']
         cam2img = input_dict['cam2img']
 
         centers_2d_with_depth = points_cam2img(
-            gt_bboxes_3d.bottom_center, cam2img, with_depth=True)
+            bboxes_3d.bottom_center, cam2img, with_depth=True)
 
         input_dict['centers_2d'] = centers_2d_with_depth[:, :2]
         input_dict['depths'] = centers_2d_with_depth[:, 2]
@@ -39,17 +40,36 @@ class BottomCenterToCenters2DWithDepth(BaseTransform):
 class ObjectPlaneAlignment(BaseTransform):
 
     def transform(self, input_dict: dict) -> dict:
-        gt_bboxes_3d = input_dict['gt_bboxes_3d']
+        bboxes_3d = input_dict['gt_bboxes_3d']
         cam2img = input_dict['cam2img']
         plane = input_dict['plane']
 
-        gt_bboxes_3d.tensor[:, 4] += plane[3] - gt_bboxes_3d.bottom_height
-        gt_bboxes_3d.tensor[:, 1] = plane[3]
+        bboxes_3d.tensor[:, 4] += plane[3] - bboxes_3d.bottom_height
+        bboxes_3d.tensor[:, 1] = plane[3]
 
         centers_2d_with_depth = points_cam2img(
-            gt_bboxes_3d.gravity_center, cam2img, with_depth=True)
+            bboxes_3d.gravity_center, cam2img, with_depth=True)
 
         input_dict['centers_2d'] = centers_2d_with_depth[:, :2]
         input_dict['depths'] = centers_2d_with_depth[:, 2]
+
+        return input_dict
+
+
+@TRANSFORMS.register_module()
+class ObjectShiftHeight(BaseTransform):
+
+    def transform(self, input_dict: dict) -> dict:
+        bboxes_3d = input_dict['gt_bboxes_3d']
+        plane = input_dict['plane']
+        box_type_3d = input_dict['box_type_3d']
+
+        shift_height = plane[3] - bboxes_3d.bottom_height
+        bboxes_3d = box_type_3d(
+            torch.cat([bboxes_3d.tensor, shift_height], dim=1),
+            box_dim=bboxes_3d.box_dim + 1,
+            with_yaw=bboxes_3d.with_yaw)
+
+        input_dict['gt_bboxes_3d'] = bboxes_3d
 
         return input_dict
