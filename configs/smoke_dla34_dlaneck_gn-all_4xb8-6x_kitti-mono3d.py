501d967e6aff323f469e2581050f28cfc9e2aa21
josugoar <josugoar@gmail.com>
2024-06-07 04:40:17 +0200

    Update default_hooks in config files

--- configs/smoke_dla34_dlaneck_gn-all_4xb8-6x_kitti-mono3d.py
+++ configs/smoke_dla34_dlaneck_gn-all_4xb8-6x_kitti-mono3d.py
@@ -14,7 +14,8 @@ val_dataloader = dict(batch_size=8, num_workers=4)
 train_cfg = dict(val_interval=5)
 auto_scale_lr = dict(enable=True, base_batch_size=32)
 
-default_hooks = dict(checkpoint=dict(type='CheckpointHook', interval=5))
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', interval=5, max_keep_ckpts=-1))
 
 vis_backends = [dict(type='TensorboardVisBackend')]
 visualizer = dict(
