4edb1ad73b8175ba6fe6941a48c552e9b0669534
josugoar <josugoar@gmail.com>
2024-06-10 21:15:16 +0200

    Correct evaluation projection

--- monogp/ground_plane_kitti_metric.py
+++ monogp/ground_plane_kitti_metric.py
@@ -1,11 +1,22 @@
-from typing import List
+from typing import List, Tuple
+
+import numpy as np
 
 from mmdet3d.evaluation import KittiMetric
 from mmdet3d.registry import METRICS
+from mmdet3d.structures import points_cam2img
+from .utils import points_img2plane
 
 
 @METRICS.register_module()
 class GroundPlaneKittiMetric(KittiMetric):
+
+    def __init__(self,
+                 *args,
+                 origin: Tuple[float, float, float] = (0.5, 0.5, 0.5),
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.origin = origin
 
     def convert_annos_to_kitti_annos(self, data_infos: dict) -> List[dict]:
         data_annos = super().convert_annos_to_kitti_annos(data_infos)
@@ -15,11 +26,25 @@ class GroundPlaneKittiMetric(KittiMetric):
                 if len(annos['instances']) == 0:
                     continue
 
+                cam2img = annos['images'][self.default_cam_key]['cam2img']
                 plane = annos['plane']
                 kitti_annos = annos['kitti_annos']
+                dimensions = kitti_annos['dimensions']
                 location = kitti_annos['location']
 
-                location = plane[3] / location[:, 1:2] * location
+                dst = np.array((0.5, 1.0, 0.5))
+                src = np.array(self.origin)
+
+                centers_3d = location - dimensions * (dst - src)
+                centers_2d = points_cam2img(centers_3d, cam2img)
+
+                location = points_img2plane(
+                    centers_2d,
+                    dimensions[:, 1],
+                    cam2img,
+                    plane,
+                    origin=self.origin)
+                location += dimensions * (dst - src)
 
                 kitti_annos['location'] = location
 
