5e14a5ec0bdbd5e666dd5ca004cff79dd5492bf6
josugoar <josugoar@gmail.com>
2024-06-12 15:47:40 +0200

    Add correct resize to pipeline

--- configs/fcos3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
+++ configs/fcos3d-bbox2d-kpts_r101-caffe-dcn_fpn_head-gn_4xb3-4x_kitti-mono3d.py
@@ -19,6 +19,25 @@ model = dict(
 
 backend_args = None
 
+train_pipeline = [
+    dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
+    dict(
+        type='LoadAnnotations3D',
+        with_bbox=True,
+        with_label=True,
+        with_attr_label=False,
+        with_bbox_3d=True,
+        with_label_3d=True,
+        with_bbox_depth=True),
+    dict(type='Resize3D', scale=(1242, 375), keep_ratio=True),
+    dict(type='RandomFlip3D', flip_ratio_bev_horizontal=0.5),
+    dict(
+        type='Pack3DDetInputs',
+        keys=[
+            'img', 'gt_bboxes', 'gt_bboxes_labels', 'attr_labels',
+            'gt_bboxes_3d', 'gt_labels_3d', 'centers_2d', 'depths'
+        ]),
+]
 tta_pipeline = [
     dict(type='LoadImageFromFileMono3D', backend_args=backend_args),
     dict(
@@ -57,7 +76,8 @@ tta_pipeline = [
         ])
 ]
 
-train_dataloader = dict(batch_size=8, num_workers=4)
+train_dataloader = dict(
+    batch_size=8, num_workers=4, dataset=dict(pipeline=train_pipeline))
 val_dataloader = dict(batch_size=8, num_workers=4)
 
 train_cfg = dict(val_interval=2)
